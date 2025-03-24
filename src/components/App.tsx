9222e8e42014166923657c8fdfb67fa0e9bb0510
Fircrey <pachonsuarezjesus@gmail.com>
2025-03-23 23:22:11 -0500

    icono-gif de carga v2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../services/graphql";
-import PokemonList from "./PokemonList";
-import Navbar from "./Navbar";
-import Buscador from "./Buscador";
+import Inicio from "./Pages/Inicio";
+import Pokedex from "./Pages/Pokedex";
 
 const App = () => {
-  const [filtro, setFiltro] = useState("");
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <Routes>
+                    {/* Página de inicio */}
+                    <Route path="/" element={<Inicio />} />
 
-  return (
-    <ApolloProvider client={client}>
-      <>
-        <Navbar />
-        <Buscador onSearch={setFiltro} />
-        <PokemonList filtro={filtro} />
-      </>
-    </ApolloProvider>
-  );
+                    {/* Página de la Pokédex */}
+                    <Route path="/pokedex" element={<Pokedex />} />
+                </Routes>
+            </Router>
+        </ApolloProvider>
+    );
 };
 
 export default App;
-/* Prueba de cambiotikitiki */
